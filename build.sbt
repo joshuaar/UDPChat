24dae804381d1712321984ee276d7d92d865e4a6
Josh <joshamosricher@gmail.com>
2013-06-06 13:45:54 -0700

    Functional chat, tested on local.

--- build.sbt
+++ build.sbt
@@ -1,3 +1,7 @@
+import AssemblyKeys._ // put this at the top of the file
+
+assemblySettings
+
 name := "UDPChat"
 
 version := "0.1"
--- src/main/scala/Chat.scala
+++ src/main/scala/Chat.scala
@@ -1,35 +1,95 @@
+package chat.udp
 import akka.actor._
 import java.util.Random
 import scala.math
 import java.net.DatagramPacket
 import java.net.DatagramSocket
-
+import java.net.SocketException
+import java.net.InetAddress
 
 abstract class msg
 case class MESSAGE(from:String,msg:String) extends msg
 case class SUCCESS extends msg
 case class STOP extends msg
+case class LISTEN extends msg
+case class PUNCH(ip:InetAddress)
+case class SEND(data:String,ip:InetAddress,port:Int)
+case class PACKET(data:DatagramPacket)
 
-object udp { 
-  def echo() = {
-    println("starting")
-  val bufsize = 16 
-  val port = 4444
-  val sock = new DatagramSocket(port)
-  val buf = new Array[Byte](bufsize)
-  val packet = new DatagramPacket(buf, bufsize)
-  while (true) {
-    println("looping")
-    sock.receive(packet)
-    println("received packet from: " + packet.getAddress())
-    sock.send(packet)
-    println("echoed data (first 16 bytess): " +
-            packet.getData().take(16).toString())
+/**
+ * Simple class for sending and receiving UDP packets
+ */
+class udp(pt:Int, nBytes:Int=2048) { 
+  val port = pt
+  val sock = new DatagramSocket(pt)  
+  /**
+   * Listens for a single packet on a socket
+   */
+  def listen():DatagramPacket={
+    val packet = new Array[Byte](nBytes)
+    val receivePacket = new DatagramPacket(packet,packet.length)
+    sock.receive(receivePacket)
+    return receivePacket
+  }
+  /**
+   * Sends a packet to a remote host
+   */
+  def send(data:Array[Byte],IP:InetAddress,port:Int){
+    val toSend = new DatagramPacket(data,data.length,IP,port)
+    sock.send(toSend)
   }
 }
 
+/**
+ * An actor that listens on a UDP socket.
+ * args: 
+ * 	socket: A udp socket object
+ */
+class UDPListener(socket:udp) extends Actor {
+  val sock = socket
+  def receive = {
+    case LISTEN => {
+      println("Listening")
+      sender ! PACKET(sock.listen())
+      println("Got and sent")
+      }
+  }
 }
 
+class UDPActor(socket:udp,printer:ActorRef) extends Actor {
+  
+  //Number of retransmissions during punching
+  val retransmit = 60
+  
+  import context._
+  //Start up the listener
+  val listener = actorOf(Props(new UDPListener(socket)), name="listener")
+  listener ! LISTEN
+  
+  def receive = {
+    //Got a packet from the listener
+    case PACKET(data) => {
+      listener ! LISTEN
+      printer ! new MESSAGE("default",new String(data.getData))
+      }
+    
+    //Got a request to send a packet somewhere
+    case SEND(data,ip,port) => {
+      println("Sending")
+      socket.send(data.getBytes,ip,port)
+      println("Done sending")
+      }
+    
+    //Spray packets at a remote host to punch hole in local NAT
+    case PUNCH(ip) => {
+      for(i <- 1 to retransmit) {
+        self ! SEND("HP_REQ",ip,socket.port)
+      }
+    }//end case PUNCH
+  }
+}
+
+//Print messages to the screen
 class MsgPrinter extends Actor {
   def receive = {
     case MESSAGE(from,msg) =>
@@ -49,14 +109,18 @@ class MsgSender(printer:ActorRef) extends Actor {
 }
 
 object Main extends App{
-  udp.echo()
   println("Starting")
+  val pt = 6005
+  val lh = InetAddress.getByName("localhost")
+  val sock = new udp(pt)
   val system = ActorSystem("ChatSystem")
   val printer = system.actorOf(Props[MsgPrinter], name = "printer")
-  val sender = system.actorOf(Props(new MsgSender(printer)), name = "sender")
+  val sender = system.actorOf(Props(new UDPActor(sock,printer)), name = "sender")
   println("Looping")
-  // start input loop
+  sender ! PUNCH(lh)
   while(true){
-    printer ! MESSAGE("Josh",readLine)
+    sender ! SEND(readLine,lh,pt)
   }
+  
+
 }
